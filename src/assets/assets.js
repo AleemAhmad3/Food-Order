bfc94bcb18adc7ff1f9064e6944affee0c9d7b20
AleemAhmad3 <ahmadaleem592@gmail.com>
2024-08-26 12:04:51 +0500

    updated files

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,4 @@
-import basket_icon from './img/basket_icon.png';
-import logo from './img/logo.png';
-import header_img from './img/header_img.png';
-import search_icon from './img/search_icon.png';
+
 import menu_1 from './img/menu_1.png';
 import menu_2 from './img/menu_2.png';
 import menu_3 from './img/menu_3.png';
@@ -44,43 +41,6 @@ import food_30 from './img/food_30.png';
 import food_31 from './img/food_31.png';
 import food_32 from './img/food_32.png';
 
-import add_icon_white from './img/add_icon_white.png';
-import add_icon_green from './img/add_icon_green.png';
-import remove_icon_red from './img/remove_icon_red.png';
-import app_store from './img/app_store.png';
-import play_store from './img/play_store.png';
-import linkedin_icon from './img/linkedin_icon.png';
-import facebook_icon from './img/facebook_icon.png';
-import twitter_icon from './img/twitter_icon.png';
-import cross_icon from './img/cross_icon.png';
-import selector_icon from './img/selector_icon.png';
-import rating_starts from './img/rating_starts.png';
-import profile_icon from './img/profile_icon.png';
-import bag_icon from './img/bag_icon.png';
-import logout_icon from './img/logout_icon.png';
-import parcel_icon from './img/parcel_icon.png';
-
-export const assets = {
-    logo,
-    basket_icon,
-    header_img,
-    search_icon,
-    rating_starts,
-    add_icon_green,
-    add_icon_white,
-    remove_icon_red,
-    app_store,
-    play_store,
-    linkedin_icon,
-    facebook_icon,
-    twitter_icon,
-    cross_icon,
-    selector_icon,
-    profile_icon,
-    logout_icon,
-    bag_icon,
-    parcel_icon
-}
 
 export const menu_list = [
     {
